aed8803765b1dc27305ba00802c4ee8cd73a7656
Haydara <haydaraal2005@gmail.com>
2025-11-28 20:12:47 +0100

    Added input and move logic to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,38 @@
 using UnityEngine;
 
-public class Player
+public class Player : MonoBehaviour
 {
-    
+    [SerializeField] private float moveSpeed = 7f;
+
+    private void Update()
+    {
+        Vector2 inputVector = new(0, 0);
+
+        // GetKey is getting the key for multiple frames i.e moving
+        // GetKeyDown gets the key for one frame i.e jumping
+        if(Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new(inputVector.x, 0f, inputVector.y);
+        transform.position += moveSpeed * Time.deltaTime * moveDir;
+    }
 }
